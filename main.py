3660996523728ea02a7fb104f743c632783925e8
arkan.alexei <arkan.alexei@ui.ac.id>
2025-07-15 22:32:54 +0700

    feat: used classify_tool in main

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional
+from dotenv import load_dotenv
 
+from agent import classify_tool
 from tools import math_tool, weather_tool, llm_tool
+
+load_dotenv()
 
 app = FastAPI()
 
 
 class QueryRequest(BaseModel):
   query: str
-  tool: Optional[str] = None # temporary, to test tool usage
 
 @app.post("/query")
 async def handle_query(request: QueryRequest):
-  tool_used = request.tool if request.tool else "none"
+  tool_used = classify_tool(request.query)
   
   if tool_used == "math":
     result = math_tool.run(request.query)
+    
+    if "Error" in result:  # Fallback to LLM if math fails
+      tool_used = "llm"
+      result = llm_tool.run(request.query)
+      
   elif tool_used == "weather":
     result = weather_tool.run(request.query)
-  elif tool_used == "llm":
-    result = llm_tool.run(request.query)
   else:
-    result = "Tool not used yet"
+    result = llm_tool.run(request.query)
   
   return {
     "query": request.query,
